d0346c8fb9ddb6ef6c17c5313198836ab645cdd9
AdnanHasan21 <majeduladnan.fbook@gmail.com>
2020-03-04 19:52:18 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+/* let name;
+console.log(name);  variable name diye value set na korle undefined hoy */
+
+//function add(num1,num2){
+//     sum = num1+num2; 
+// }       
+// const total = add (2,3);    function e value return na korle undefined ashe
+// console.log(total);
+
+//function add(num1,num2){
+//     sum = num1+num2; 
+// }       
+// const total = add (2);  function e parameter diye sheta pass na korle undefined ashe + return na korle alada undefined ashe  
+// console.log(total);
+
+// const age = [10,20,30];
+// console.log(age[8]);    array te 3 ta element ache. othocho jante cheyechi 8th element er kotha.
